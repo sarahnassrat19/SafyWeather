b72e7d70771e00535a2a6d19c521348baf360bef
Sarah Nassrat <sarahnassrat2017@gmail.com>
2022-05-05 01:22:10 +0200

    home screen UI

---
+++ app/src/main/java/com/example/safyweather/homescreen/DailyWeatherAdapter.kt
@@ -0,0 +1,58 @@
+package com.example.safyweather.homescreen
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.safyweather.R
+
+class DailyWeatherAdapter:RecyclerView.Adapter<DailyWeatherAdapter.DailyWeatherViewHolder> {
+
+    private var context: Context
+    //private var dailyWeather:List<DailyWeather>
+
+    constructor(context: Context/*,dailyWeather:List<DailyWeather>*/){
+        this.context = context
+        //this.dailyWeather = dailyWeather
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): DailyWeatherAdapter.DailyWeatherViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.daily_item_layout,parent,false)
+        return DailyWeatherViewHolder(view)
+    }
+
+    override fun onBindViewHolder(
+        holder: DailyWeatherAdapter.DailyWeatherViewHolder,
+        position: Int
+    ) {
+        /*val oneDailyWeather:DailyWeather = dailyWeather[position]
+        holder.dailyDate.text = oneDailyWeather.date
+        holder.dailyDesc.text = oneDailyWeather.desc
+        holder.dailyTemp.text = oneDailyWeather.temp
+        holder.dailyIcon.setImageResource(R.drawable.daily_icon)*/
+    }
+
+    override fun getItemCount(): Int {
+        //return dailyWeather.size
+        return 0
+    }
+
+    inner class DailyWeatherViewHolder(private val view: View):RecyclerView.ViewHolder(view){
+        val dailyDate:TextView
+            get() =view.findViewById(R.id.dailyDate)
+        val dailyDesc:TextView
+            get() =view.findViewById(R.id.dailyDesc)
+        val dailyTemp:TextView
+            get() =view.findViewById(R.id.dailyTemp)
+        val dailyIcon:ImageView
+            get() = view.findViewById(R.id.dailyIcon)
+    }
+}
---
+++ app/src/main/java/com/example/safyweather/homescreen/HourlyWeatherAdapter.kt
@@ -0,0 +1,57 @@
+package com.example.safyweather.homescreen
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.safyweather.R
+
+class HourlyWeatherAdapter : RecyclerView.Adapter<HourlyWeatherAdapter.HourlyWeatherViewHolder>{
+
+    private var context: Context
+    //private var hourlyWeather:List<HourlyWeather>
+
+    constructor(context: Context/*,hourlyWeather:List<HourlyWeather>*/){
+        this.context = context
+        //this.hourlyWeather = hourlyWeather
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): HourlyWeatherAdapter.HourlyWeatherViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.hourly_item_layout,parent,false)
+        return HourlyWeatherViewHolder(view)
+    }
+
+    override fun onBindViewHolder(
+        holder: HourlyWeatherAdapter.HourlyWeatherViewHolder,
+        position: Int
+    ) {
+        /*val oneHourlyWeather:HourlyWeather = hourlyWeather[position]
+        holder.hourlyTime.text = oneHourlyWeather.time
+        holder.hourlyTemp.text = oneHourlyWeather.temp
+        holder.hourlyWindSpeed.text = oneHourlyWeather.windSpeed
+        holder.hourlyIcon.setImageResource(R.drawable.hourly_icon)*/
+    }
+
+    override fun getItemCount(): Int {
+        //return hourlyWeather.size
+        return 0
+    }
+
+    inner class HourlyWeatherViewHolder(private val view: View):RecyclerView.ViewHolder(view){
+        val hourlyTime:TextView
+        get() = view.findViewById(R.id.hourlyTime)
+        val hourlyTemp:TextView
+        get() = view.findViewById(R.id.hourlyTemp)
+        val hourlyWindSpeed:TextView
+        get() = view.findViewById(R.id.hourlyWindSpeed)
+        val hourlyIcon:ImageView
+        get() = view.findViewById(R.id.hourlyIcon)
+    }
+
+}
